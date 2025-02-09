a0afd9209a090527bf5b0d4e6f052934dfcb1a34
Sheikh Minhajul Abedin <nion.sk5067@gmail.com>
2025-02-09 18:06:01 +0000

    object running

---
+++ object.js
@@ -0,0 +1,10 @@
+//object => key value payer
+const car = {
+  name: 'BMW',
+  color: 'black',
+  style: 'new',
+  accelerate: function () {
+    console.log("speed increases")
+  }
+}
+car.accelerate();
