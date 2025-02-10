300663caf192fbd430d60972dd7564e47bc5d65b
Sheikh Minhajul Abedin <nion.sk5067@gmail.com>
2025-02-10 11:51:07 +0000

    "finish tutorial"

--- object.js
+++ object.js
@@ -8,3 +8,21 @@ const car = {
   }
 }
 car.accelerate();
+
+const mine = {
+  name: 'shahin',
+  favColor:'white',
+  'favourite place':['mosque', 'coxs-bazar', 'rangamati', 'jaflong' ]
+}
+mine.favColor = 'green';
+console.log(mine);
+
+console.log(mine['favourite place']);
+const keys = Object.keys(mine) //to access the properties
+console.log(keys)
+console.log(Object.values(mine)) //to access the values
+//to add the property
+mine.isMarried = true;
+mine.age = 33;
+mine.occupation='web developer'
+console.log("about myself: ", mine)
